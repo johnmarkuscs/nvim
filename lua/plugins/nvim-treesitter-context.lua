990d426028e7616fe089cf74b8f79a42f2fd9289
johnmarkuscs <markuscabrera.cs@gmail.com>
2024-01-16 14:56:20 -0500

    Latest

--- lua/plugins/nvim-treesitter-context.lua
+++ lua/plugins/nvim-treesitter-context.lua
@@ -2,5 +2,13 @@ return {
     "nvim-treesitter/nvim-treesitter-context",
     config = function()
         require("treesitter-context").setup()
+
+        vim.api.nvim_exec([[
+            hi TreesitterContext guibg=none
+            hi TreesitterContextLineNumber guibg=none
+            hi TreesitterContextSeparator guibg=none
+            hi TreesitterContextBottom gui=none guisp=Grey
+        ]], false)
     end,
+
 }
